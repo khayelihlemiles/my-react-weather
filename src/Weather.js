3067afb1f7690806e1e41fd284c4add0534435d3
khayelihlemiles <mileskhayelihle00@gmail.com>
2023-12-06 22:32:34 +0200

    added axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,56 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 import "./weather.css";
+import axios from "axios";
 
 export default function Weather() {
-  return (
-    <div className="weather">
-      <form>
-        <input type="text" placeholder="Enter a city.." className="type" />
-        <input type="submit" value="search" className="button" />
-      </form>
-      <h1>Sydney</h1>
-      <ul>
-        <li>
-          Monday 22:36, <strong>Slight rainfall</strong>
-          <li>Humidity:56%, Wind speed:2.1km/hr</li>
-        </li>
-      </ul>
-      <h2>🌦25°c</h2>
-      <div className="weekdays">
-        <div>
-          🌧
-          <br />
-          Tue
-          <br />
-          22°|<strong>15°</strong>
-        </div>
-        <div>
+  const { ready, setReady } = useState(false);
+  const { weatherData, setWeatherData } = useState({});
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      temperature: response.data.temperature.current,
+      wind: 12,
+      city: response.data.city,
+      humidity: response.data.temperature.humidity,
+      description: response.data.condition.description,
+    });
+
+    setReady(true);
+  }
+  if (ready) {
+    return (
+      <div className="weather">
+        <form>
+          <input type="text" placeholder="Enter a city.." className="type" />
+          <input type="submit" value="search" className="button" />
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>
+            Monday 22:36, <strong>{weatherData.description}</strong>
+            <li>
+              Humidity:{weatherData.humidity}, Wind speed:{weatherData.wind}/hr
+            </li>
+          </li>
+        </ul>
+        <h2>
           🌦
-          <br />
-          Wed
-          <br />
-          14°|<strong>31°</strong>
-        </div>
-        <div>
-          ☀<br />
-          Thu
-          <br />
-          16°|<strong>33°</strong>
-        </div>
-        <div>
-          ☀<br />
-          Fri
-          <br />
-          13°|<strong>34°</strong>
-        </div>
-        <div>
-          🌧
-          <br />
-          Sat
-          <br />
-          16°|<strong>22°</strong>
+          <span className="temperature">
+            {Math.round(weatherData.temperature)}
+          </span>
+          °c
+        </h2>
+        <div className="weekdays">
+          <div>
+            🌧
+            <br />
+            Tue
+            <br />
+            22°|<strong>15°</strong>
+          </div>
+          <div>
+            🌦
+            <br />
+            Wed
+            <br />
+            14°|<strong>31°</strong>
+          </div>
+          <div>
+            ☀<br />
+            Thu
+            <br />
+            16°|<strong>33°</strong>
+          </div>
+          <div>
+            ☀<br />
+            Fri
+            <br />
+            13°|<strong>34°</strong>
+          </div>
+          <div>
+            🌧
+            <br />
+            Sat
+            <br />
+            16°|<strong>22°</strong>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "79be4f630f69t8bffa4o15edaf3814a6";
+    let city = "Sydney";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "loading...";
+  }
 }
