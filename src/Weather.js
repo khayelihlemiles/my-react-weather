85421d05be89420dbfa6671bf0acae35e11a38a7
khayelihlemiles <mileskhayelihle00@gmail.com>
2023-12-13 01:02:38 +0200

    added a weather search engin

--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
-import "./weather.css";
-import currentdate from "./currentdate";
 import axios from "axios";
+import "./weather.css";
 
-export default function Weather() {
-  const [ready, setReady] = useState(false);
+import WeatherInfo from "./weatherinfo";
+
+export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
   function handleResponse(response) {
-    console.log(response.data);
     setWeatherData({
+      ready: true,
       temperature: response.data.temperature.current,
       wind: response.data.wind.speed,
       city: response.data.city,
@@ -17,77 +18,39 @@ export default function Weather() {
       iconUrl: response.data.condition.icon_url,
       date: new Date(response.data.time * 1000),
     });
-
-    setReady(true);
   }
+  function search() {
+    const apiKey = "79be4f630f69t8bffa4o15edaf3814a6";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
   if (ready) {
     return (
       <div className="weather">
-        <form>
-          <input type="text" placeholder="Enter a city.." className="type" />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Enter a city.."
+            className="type"
+            onChange={handleCityChange}
+          />
           <input type="submit" value="search" className="button" />
         </form>
-        <h1>{weatherData.city}</h1>
-        <ul>
-          <li>
-            <currentdate date={weatherData.date} />
-            <strong>{weatherData.description}</strong>
-            <li>
-              Humidity:{weatherData.humidity}, Wind speed:{weatherData.wind}
-              km/hr
-            </li>
-          </li>
-        </ul>
-        <h2>
-          <img src={weatherData.iconUrl} alt={weatherData.description} />
-          <span className="temperature">
-            {Math.round(weatherData.temperature)}
-          </span>
-          °c
-        </h2>
-        <div className="weekdays">
-          <div>
-            🌧
-            <br />
-            Tue
-            <br />
-            22°|<strong>15°</strong>
-          </div>
-          <div>
-            🌦
-            <br />
-            Wed
-            <br />
-            14°|<strong>31°</strong>
-          </div>
-          <div>
-            ☀<br />
-            Thu
-            <br />
-            16°|<strong>33°</strong>
-          </div>
-          <div>
-            ☀<br />
-            Fri
-            <br />
-            13°|<strong>34°</strong>
-          </div>
-          <div>
-            🌧
-            <br />
-            Sat
-            <br />
-            16°|<strong>22°</strong>
-          </div>
-        </div>
+        <WeatherInfo data={weatherData} />
       </div>
     );
   } else {
-    const apiKey = "79be4f630f69t8bffa4o15edaf3814a6";
-    let city = "Berlin";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-
+    search();
     return "loading...";
   }
 }
---
+++ src/weatherinfo.js
@@ -0,0 +1,62 @@
+import React from "react";
+import CurrentDate from "./CurrentDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <CurrentDate date={props.data.date} />
+          <strong>{props.data.description}</strong>
+          <li>
+            Humidity:{props.data.humidity}, Wind speed:{props.data.wind}
+            km/hr
+          </li>
+        </li>
+      </ul>
+      <h2>
+        <img src={props.data.iconUrl} alt={props.data.description} />
+        <span className="temperature">
+          {Math.round(props.data.temperature)}
+        </span>
+        °c
+      </h2>
+      <div className="weekdays">
+        <div>
+          🌧
+          <br />
+          Tue
+          <br />
+          22°|<strong>15°</strong>
+        </div>
+        <div>
+          🌦
+          <br />
+          Wed
+          <br />
+          14°|<strong>31°</strong>
+        </div>
+        <div>
+          ☀<br />
+          Thu
+          <br />
+          16°|<strong>33°</strong>
+        </div>
+        <div>
+          ☀<br />
+          Fri
+          <br />
+          13°|<strong>34°</strong>
+        </div>
+        <div>
+          🌧
+          <br />
+          Sat
+          <br />
+          16°|<strong>22°</strong>
+        </div>
+      </div>
+    </div>
+  );
+}
